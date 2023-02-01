41c09b63fcbe16fdc3de4c294727f275ad2ec665
andrebiaggio <biaggioan@gmail.com>
2023-02-01 00:44:01 -0300

    deploy

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,6 @@ const userController = require("./controller/userController");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.status().json({ hello: "word" });
-});
 router.get("/users", userController.getAllUsers);
 router.get("/users/:username/details", userController.getUserDetails);
 router.get("/users/:username/repos", userController.getUserRepos);
